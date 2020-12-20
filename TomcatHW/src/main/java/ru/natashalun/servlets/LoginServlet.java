8b8247d632475420131932ed948601018940ebad
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-12-20 19:05:52 +0300

    Update LoginServlet.java

--- TomcatHW/src/main/java/ru/natashalun/servlets/LoginServlet.java
+++ TomcatHW/src/main/java/ru/natashalun/servlets/LoginServlet.java
@@ -2,11 +2,13 @@ package ru.natashalun.servlets;
 
 import ru.natashalun.service.UserStorage;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
 
@@ -15,27 +17,23 @@ public class LoginServlet extends HttpServlet {
 
         final String username = request.getParameter("username");
         final String password = request.getParameter("password");
-
         boolean isUserExist = UserStorage.isUserExist(username);
         boolean isPasswordCorrect = false;
-        String message = "";
+        PrintWriter write = response.getWriter();
 
         if (isUserExist) {
             isPasswordCorrect = UserStorage.isPasswordCorrect(username, password);
-            if (!isPasswordCorrect) {
-                message = "incorrectPassword";
-            }
-        } else {
-            message = "userNotExist";
         }
 
         request.setAttribute("username", username);
-        request.setAttribute("message", message);
 
-        if (isUserExist && isPasswordCorrect) {
-            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        if (!isUserExist) {
+            write.println("User doesn't exist. Please, sign up to proceed");
+        } else if (!isPasswordCorrect) {
+            write.print("Invalid password. Try again.");
         } else {
-            request.getRequestDispatcher("/login.jsp").forward(request, response);
+            request.getRequestDispatcher("/success.jsp").forward(request, response);
         }
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 }
