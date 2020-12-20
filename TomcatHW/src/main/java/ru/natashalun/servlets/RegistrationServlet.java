74c61d20470feb894e5ae3ccb9f3f88d4830f696
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-12-20 19:05:07 +0300

    Update RegistrationServlet.java

--- TomcatHW/src/main/java/ru/natashalun/servlets/RegistrationServlet.java
+++ TomcatHW/src/main/java/ru/natashalun/servlets/RegistrationServlet.java
@@ -7,31 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/registration.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         final String username = request.getParameter("username");
         final String password = request.getParameter("password");
 
-        if (isValidUser(username)) {
+        if (isValidUser(username, response)) {
             request.getRequestDispatcher("/registration.jsp").forward(request, response);
         } else {
             UserStorage.add(username, password);
-            response.sendRedirect("/login.jsp");
+            request.getRequestDispatcher("/success.jsp").forward(request, response);
         }
     }
 
-    private boolean isValidUser(String username) {
+    private boolean isValidUser(String username, HttpServletResponse response) throws IOException {
 
         if (username.equals("admin")) {
+            PrintWriter write = response.getWriter();
+            write.print("Login cannot be 'admin'");
             return false;
         }
         return UserStorage.isUserExist(username);
